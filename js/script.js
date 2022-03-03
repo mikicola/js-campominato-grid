b1bac0dea999c9844081fcd6bebe3e59a7188129
mikicola <michela.colangelo@gmail.com>
2022-03-03 15:53:33 +0100

    fixes css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+const idLevel = document.getElementById('level');
+const btnPlay = document.querySelector('.btn-play');
 const gameArea = document.querySelector('.game-area');
 
-for(let i = 1; i <= 100; i++){
-    square = document.createElement("div");
-    square.classList.add("square");
-    gameArea.append(square);
-    square.innerHTML = i;
+const gameArea = document.querySelector('.game-area');
+
+btnPlay.addEventListener('click', setupGame);
+
+arrLevels = [100, 81, 49]
+
+function setupGame(){
+    let indexLevel = parseFloat(idLevel.value);
 }
+
+
+
+
+
+// for(let i = 1; i <= 100; i++){
+//     square = document.createElement("div");
+//     square.classList.add("square");
+//     gameArea.append(square);
+//     square.innerHTML = i;
+// }
