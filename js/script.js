0f546bd5b831faa6381426c8a3d10805de1d1f85
mikicola <michela.colangelo@gmail.com>
2022-03-06 19:07:02 +0100

    script completato

--- js/script.js
+++ js/script.js
@@ -1,24 +1,79 @@
 const idLevel = document.getElementById('level');
 const btnPlay = document.querySelector('.btn-play');
 const gameArea = document.querySelector('.game-area');
+let level = document.getElementById('level');
 
-const gameArea = document.querySelector('.game-area');
+btnPlay.addEventListener('click', startGame);
 
-btnPlay.addEventListener('click', setupGame);
 
-arrLevels = [100, 81, 49]
 
-function setupGame(){
-    let indexLevel = parseFloat(idLevel.value);
+function selectedSquareChangeColor(){
+    this.classList.add('square-selected-color');
+}
+
+function startGame() {
+    
+    gameArea.innerHTML = '';
+    if(level.value == 'Easy') {
+        for (let i = 1; i <= 100; i++) {
+             let square = document.createElement("div");
+            square.classList.add("square");
+            gameArea.append(square);
+            square.innerHTML = i;
+
+            square.addEventListener('click', selectedSquareChangeColor);
+            console.log(i)
+        }
+
+    }else if (level.value == 'Medium') {
+        for (let i = 1; i <= 81; i++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            gameArea.append(square);
+            square.innerHTML = i;
+
+            square.addEventListener('click', selectedSquareChangeColor);
+            console.log(i)
+        }
+    }else if (level.value == 'Hard') {
+        for (let i = 1; i <= 49; i++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            gameArea.append(square);
+            square.innerHTML = i;
+
+            square.addEventListener('click', selectedSquareChangeColor);
+            console.log(i)
+        }
+    }
 }
 
 
 
 
 
-// for(let i = 1; i <= 100; i++){
-//     square = document.createElement("div");
-//     square.classList.add("square");
-//     gameArea.append(square);
-//     square.innerHTML = i;
-// }
+
+
+
+
+
+
+
+
+
+
+/* prova che non mi ha funzionato :(
+arrLevels = [100, 81, 49];
+
+function startGame(){
+    const indexLevel = parseInt(idLevel.value); //creata qui
+    console.log('indexLevel', indexLevel);
+    
+    let cellsCount = arrLevels[arrLevels]; //creata qui
+    console.log('cellsCount', cellsCount);
+    
+    for(let cellNum = 1; cellNum <= cellsCount; cellNum ++){
+    }
+    console.log(cellNum);
+}
+*/
